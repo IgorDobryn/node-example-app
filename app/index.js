389e654d27f74d5c7cd3f5b19baf7151a178920a
Igor Dobryn <IgorDobryn@gmail.com>
2018-05-25 08:53:43 +0300

    Add simple positive test case for general data fetching

--- app/index.js
+++ app/index.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 const { getApiStream } = require('./ApiStream');
 const { json2csvStream } = require('./JsonToCsvStream');
 
-const exportApiDataToCsv = (apiStreamOpts, outFileName) => {
+const exportApiDataToCsv = (apiStreamOpts, outFileName, callback = () => {}) => {
   const fileStream = fs.createWriteStream(outFileName);
   getApiStream(apiStreamOpts)
+    .on('finish', callback)
     .pipe(json2csvStream)
     .pipe(fileStream);
 }
---
+++ test/index_test.js
@@ -0,0 +1,80 @@
+const stringToStream = require('string-to-stream');
+const fs = require('fs');
+const nock = require('nock');
+const { expect, use } = require("chai");
+const { exportApiDataToCsv } = require('../app/index');
+
+use(require('chai-fs'));
+
+describe('exportApiDataToCsv', () => {
+  const host = 'http://localhost:9292';
+  const path = '/MyUrl.json';
+  const url = host + path;
+  const auth = {
+    user: 'MyUser',
+    password: 'MyPassword',
+  };
+  const outFile = './test.csv';
+
+  const user = {
+    id: 1002,
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'jd@jd.com',
+    gender: 'Male',
+    ip_address: '10.10.10.10',
+    address: {
+      street: 'Street',
+      City: 'City',
+      State: 'Nevada',
+      Country: 'United States'
+    }
+  }
+
+
+  beforeEach(() => {
+    if (fs.existsSync(outFile)) {
+      fs.unlink(outFile);
+    }
+
+    nock(host)
+      .get(path)
+      .basicAuth({
+        user: auth.user,
+        pass: auth.password,
+      })
+      .query({
+        page_number: 1,
+      })
+      .reply(200, () => {
+        return stringToStream(JSON.stringify([user]));
+      });
+
+    nock(host)
+      .get(path)
+      .basicAuth({
+        user: auth.user,
+        pass: auth.password,
+      })
+      .query({
+        page_number: 2,
+      })
+      .reply(404, () => {
+        return stringToStream('No more records');
+      });
+  });
+
+  it('fetches data from API and creates csv file', () => {
+    expect(outFile).to.not.be.a.path();
+
+    exportApiDataToCsv({ ...auth, url }, outFile, () => {
+      const csv = fs.readFileSync(outFile, 'utf8');
+
+      expect(csv.split("\n")).to.eql([
+        '"id","first_name","last_name","email","gender","ip_address","address"',
+        '1002,"John","Doe","jd@jd.com","Male","10.10.10.10","Street City Nevada United States"',
+      ]);
+      done();
+    });
+  });
+});
