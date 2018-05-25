e48471f8ac3c073876111b9059cf317d60e64600
Igor Dobryn <IgorDobryn@gmail.com>
2018-05-25 08:51:01 +0300

    No need to force close the stream as closed automatically

--- app/index.js
+++ app/index.js
@@ -5,7 +5,6 @@ const { json2csvStream } = require('./JsonToCsvStream');
 const exportApiDataToCsv = (apiStreamOpts, outFileName) => {
   const fileStream = fs.createWriteStream(outFileName);
   getApiStream(apiStreamOpts)
-    .on('end', () => fileStream.end())
     .pipe(json2csvStream)
     .pipe(fileStream);
 }
